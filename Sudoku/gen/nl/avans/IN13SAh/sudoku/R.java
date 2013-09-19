d04fb8b7bd4254540325ff342aecd56d0667f7c9
Rick Veens <rickveens92@gmail.com>
2013-09-19 14:51:22 +0200

    Invoer grid toegevoegd aan spelscherm.

--- Sudoku/gen/nl/avans/IN13SAh/sudoku/R.java
+++ Sudoku/gen/nl/avans/IN13SAh/sudoku/R.java
@@ -26,11 +26,22 @@ public final class R {
     public static final class id {
         public static final int GridItem=0x7f080000;
         public static final int MyGrid=0x7f080002;
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080011;
+        public static final int button1=0x7f080004;
+        public static final int button2=0x7f080005;
+        public static final int button3=0x7f080006;
+        public static final int button4=0x7f080008;
+        public static final int button6=0x7f080009;
+        public static final int button7=0x7f08000b;
+        public static final int button8=0x7f08000c;
+        public static final int button9=0x7f08000d;
         public static final int grid_item_text=0x7f080001;
-        public static final int showinfoButton=0x7f080005;
-        public static final int startgameButton=0x7f080004;
-        public static final int textView1=0x7f080003;
+        public static final int showinfoButton=0x7f080010;
+        public static final int startgameButton=0x7f08000f;
+        public static final int tableRow1=0x7f080003;
+        public static final int tableRow2=0x7f080007;
+        public static final int tableRow3=0x7f08000a;
+        public static final int textView1=0x7f08000e;
     }
     public static final class layout {
         public static final int sudokugrid_item=0x7f030000;
