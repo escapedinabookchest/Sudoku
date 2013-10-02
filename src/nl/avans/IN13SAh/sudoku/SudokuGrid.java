5d1ca6104af474eafb40c465f2285be256437049
Rick Veens <rickveens92@gmail.com>
2013-10-02 12:06:56 +0200

    Added example canvas code.

--- src/nl/avans/IN13SAh/sudoku/SudokuGrid.java
+++ src/nl/avans/IN13SAh/sudoku/SudokuGrid.java
@@ -1,9 +1,12 @@
 package nl.avans.IN13SAh.sudoku;
 
-import nl.avans.game.Game;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,9 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,7 +31,6 @@ public class SudokuGrid extends Activity {
 	GridView MyGrid; // The gridview object that will be displayed on the
 						// screen.
 	VakjeAdapter adapter;
-	Game game = new Game(9, 1);
 	Button button1, button2, button3, button4, button5, button6, button7,
 			button8, button9;
 	Button buttonClear, buttonSolve, buttonGenerate;
@@ -38,136 +43,56 @@ public class SudokuGrid extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.sudokugrid_main); // Set the layout.
+		// We'll be creating an image that is 100 pixels wide and 200 pixels
+		// tall.
+		int width = 100;
+		int height = 200;
 
-		MyGrid = (GridView) findViewById(R.id.MyGrid);
-		adapter = new VakjeAdapter(this);
-		MyGrid.setAdapter(adapter);
+		// Create a bitmap with the dimensions we defined above, and with a
+		// 16-bit pixel format. We'll
+		// get a little more in depth with pixel formats in a later post.
+		Bitmap bitmap = Bitmap.createBitmap(width, height, Config.RGB_565);
 
-		// buttons
-		button1 = (Button) findViewById(R.id.button1);
-		button2 = (Button) findViewById(R.id.button2);
-		button3 = (Button) findViewById(R.id.button3);
-		button4 = (Button) findViewById(R.id.button4);
-		button5 = (Button) findViewById(R.id.button5);
-		button6 = (Button) findViewById(R.id.button6);
-		button7 = (Button) findViewById(R.id.button7);
-		button8 = (Button) findViewById(R.id.button8);
-		button9 = (Button) findViewById(R.id.button9);
-		buttonClear = (Button) findViewById(R.id.buttonClear);
-		buttonSolve = (Button) findViewById(R.id.buttonSolve);
-		buttonGenerate = (Button) findViewById(R.id.buttonGenerate);
+		// Create a paint object for us to draw with, and set our drawing color
+		// to blue.
+		Paint paint = new Paint();
+		paint.setColor(Color.BLUE);
 
-		// TODO selection position naar x,y converteren en invullen bij
-		// listeners.
-		button1.setOnClickListener(new OnClickListener() {
+		// Create a new canvas to draw on, and link it to the bitmap that we
+		// created above. Any drawing
+		// operations performed on the canvas will have an immediate effect on
+		// the pixel data of the
+		// bitmap.
+		Canvas canvas = new Canvas(bitmap);
 
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 1);
-				adapter.notifyDataSetChanged();
-			}
-		});
+		// Fill the entire canvas with a red color.
+		canvas.drawColor(Color.RED);
 
-		button2.setOnClickListener(new OnClickListener() {
+		// Draw a rectangle inside our image using the paint object we defined
+		// above. The rectangle's
+		// upper left corner will be at (25,50), and the lower left corner will
+		// be at (75,150). Since we set
+		// the paint object's color above, this rectangle will be blue.
+		canvas.drawRect(25, 50, 75, 150, paint);
 
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 2);
-				adapter.notifyDataSetChanged();
-			}
-		});
+		// In order to display this image in our activity, we need to create a
+		// new ImageView that we
+		// can display.
+		ImageView imageView = new ImageView(this);
 
-		button3.setOnClickListener(new OnClickListener() {
+		// Set this ImageView's bitmap to the one we have drawn to.
+		imageView.setImageBitmap(bitmap);
 
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 3);
-				adapter.notifyDataSetChanged();
-			}
-		});
+		// Create a simple layout and add our image view to it.
+		RelativeLayout layout = new RelativeLayout(this);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.CENTER_IN_PARENT);
+		layout.addView(imageView, params);
+		layout.setBackgroundColor(Color.BLACK);
 
-		button4.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 4);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button5.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 5);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button6.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 6);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button7.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 7);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button8.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 8);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button9.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 9);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		buttonClear.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				game.setCurrentValue(0, 1, 0);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		buttonSolve.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// game.solveGame(); FIXME
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		buttonGenerate.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// game.generateGame(); FIXME
-				adapter.notifyDataSetChanged();
-			}
-		});
-
+		// Show this layout in our activity.
+		setContentView(layout);
 	}
 
 	/**
@@ -198,7 +123,8 @@ public class SudokuGrid extends Activity {
 		 */
 		@Override
 		public int getCount() {
-			return game.getNumberOfColumns() * game.getNumberOfRows();
+			// return game.getNumberOfColumns() * game.getNumberOfRows();
+			return 0;
 		}
 
 		/*
@@ -221,7 +147,7 @@ public class SudokuGrid extends Activity {
 
 			// Do something with the grid view items.
 			TextView tv = (TextView) MyView.findViewById(R.id.grid_item_text);
-			tv.setText("" + game.getCurrentValue(position, position));
+			// tv.setText("" + game.getCurrentValue(position, position));
 
 			MyView.setTag(new Integer(position));
 
