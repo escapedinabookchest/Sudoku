607a37f9dbe0dfe90d84ac441c44fe4d04d8d787
Rick Veens <rickveens92@gmail.com>
2013-10-03 16:09:56 +0200

    Added a working canvas implementation. (which i totally didn't steal from the internet)

---
+++ src/nl/avans/IN13SAh/sudoku/CanvasView.java
@@ -0,0 +1,184 @@
+package nl.avans.IN13SAh.sudoku;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.FontMetrics;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+class CanvasView extends View {
+
+	private int selX;
+	private int selY;
+	private Rect selRect;
+	private int height, width;
+
+	public CanvasView(Context context) {
+		super(context);
+		selX = 0;
+		selY = 0;
+		this.selRect = new Rect();
+		setFocusable(true);
+		setFocusableInTouchMode(true);
+		// setId(PuzzleView.id)
+		Log.d("PuzzleView", "constructed");
+	}
+
+	@Override
+	protected Parcelable onSaveInstanceState() {
+		Bundle bundle = new Bundle();
+		bundle.putInt("selX", selX);
+		bundle.putInt("selY", selY);
+		bundle.putParcelable("viewState", super.onSaveInstanceState());
+
+		return bundle;
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Parcelable state) {
+		Bundle bundle = new Bundle();
+		// select(bundle.getInt("selX"), bundle.getInt("selY"))
+		bundle.getParcelable("viewState");
+
+		super.onRestoreInstanceState(state);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (event.getAction() != MotionEvent.ACTION_DOWN)
+			return super.onTouchEvent(event);
+
+		select((int) (event.getX() / width), (int) (event.getY() / height));
+
+		Log.d("PuzzleView", "onTouchEvent: #{@selX}, #{@selY}");
+		return true;
+	}
+
+	private boolean select(int x, int y) {
+		invalidate(selRect);
+		selX = Math.min(Math.max(x, 0), 8);
+		selY = Math.min(Math.max(y, 0), 8);
+		getRect(selX, selY, selRect);
+		invalidate(selRect);
+		return true;
+	}
+
+	/*
+	 * private boolean setSelectedTile(int tile) { if
+	 * (@game.setTileIfValid(@selX, @selY, tile)) { invalidate else
+	 * Log.d("PuzzleView", "setSelectedTile: invalid #{tile}")
+	 * startAnimation(AnimationUtils.loadAnimation(@game, R.anim.shake)) end }
+	 * return true; }
+	 */
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		Log.d("PuzzleView", "drawing bg");
+		// draw background
+		Paint background = loadColor(R.color.puzzle_background);
+		canvas.drawRect(0.0f, 0.0f, (float) getWidth(), (float) getHeight(),
+				background);
+
+		Log.d("PuzzleView", "drawing lines");
+		// draw board
+		// line colors
+		Paint dark = loadColor(R.color.puzzle_dark);
+		Paint hilite = loadColor(R.color.puzzle_hilite);
+		Paint light = loadColor(R.color.puzzle_light);
+
+		// draw lines
+		int i = 0;
+		while (i < 9) {
+			// minor lines
+			canvas.drawLine(0, i * height, getWidth(), i * height, light);
+			canvas.drawLine(0, i * height + 1, getWidth(), i * height + 1,
+					hilite);
+			canvas.drawLine(i * width, 0, i * width, getHeight(), light);
+			canvas.drawLine(i * width + 1, 0, i * width + 1, getHeight(),
+					hilite);
+			// major lines
+			if (i % 3 == 0) {
+				canvas.drawLine(0, i * height, getWidth(), i * height, dark);
+				canvas.drawLine(0, i * height + 1, getWidth(), i * height + 1,
+						hilite);
+				canvas.drawLine(i * width, 0, i * width, getHeight(), dark);
+				canvas.drawLine(i * width + 1, 0, i * width + 1, getHeight(),
+						hilite);
+			}
+			i++;
+		}
+
+		// draw numbers
+		Log.d("PuzzleView", "drawing numbers");
+		Paint foreground = loadColor(R.color.puzzle_foreground);
+		foreground.setStyle(Style.FILL);
+		foreground.setTextSize((float) (height * 0.75));
+		foreground.setTextScaleX(width / height);
+		foreground.setTextAlign(Paint.Align.CENTER);
+
+		FontMetrics fm = foreground.getFontMetrics();
+		float x = width / 2;
+		float y = height / 2 - (fm.ascent + fm.descent) / 2;
+		i = 0;
+		int j = 0;
+		while (i < 9) {
+			j = 0;
+			while (j < 9) {
+				canvas.drawText("test" /* game.getTileString(i, j) */, i
+						* width + x, j * height + y, foreground);
+				j++;
+			}
+			i++;
+		}
+
+		// draw hints
+		/*
+		 * if (Prefs.getHints(getContext)) colors = [
+		 * loadColor(R.color.puzzle_hint_0), loadColor(R.color.puzzle_hint_1),
+		 * loadColor(R.color.puzzle_hint_2) ] r = Rect.new i = 0 while (i<9) j =
+		 * 0 while (j<9) used =@game.getUsedTiles(i,j) moves_left = 9 -
+		 * used.size if (moves_left < colors.size) getRect(i, j, r)
+		 * canvas.drawRect(r,Paint(colors.get(moves_left))) end j+=1 end i+=1
+		 * end end
+		 */
+
+		// draw selection
+		Paint selected = loadColor(R.color.puzzle_selected);
+		canvas.drawRect(selRect, selected);
+		super.onDraw(canvas);
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		Log.d("PuzzleView", "size changed: #{w}x#{h}");
+		width = (int) (w / 9.0);
+		height = (int) (h / 9.0);
+		getRect(selX, selY, selRect);
+		super.onSizeChanged(w, h, oldw, oldh);
+	}
+
+	public Rect getRect(int x, int y, Rect rect) {
+		rect.set(x * width, y * height, x * width + width, y * height + height);
+		return rect;
+	}
+
+	public Paint loadColor(int key) {
+		Paint paint = new Paint();
+		paint.setColor(getResources().getColor(key));
+		return paint;
+	}
+
+	/*
+	 * public boolean setSelectedTile(int tile) { if
+	 * (@game.setTileIfValid(@selX, @selY, tile)) invalidate else
+	 * Log.d("PuzzleView", "setSelectedTile: invalid #{tile}")
+	 * startAnimation(AnimationUtils.loadAnimation(@game, R.anim.shake)) end
+	 * true }
+	 */
+}
--- src/nl/avans/IN13SAh/sudoku/SudokuGrid.java
+++ src/nl/avans/IN13SAh/sudoku/SudokuGrid.java
@@ -1,23 +1,16 @@
 package nl.avans.IN13SAh.sudoku;
 
+import nl.avans.game.Game;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,13 +24,7 @@ public class SudokuGrid extends Activity {
 	GridView MyGrid; // The gridview object that will be displayed on the
 						// screen.
 	VakjeAdapter adapter;
-<<<<<<< HEAD
-=======
 	Game game;
->>>>>>> b5be5a4389b1183b8c1d8bd29f2b311c48d1ae28
-	Button button1, button2, button3, button4, button5, button6, button7,
-			button8, button9;
-	Button buttonClear, buttonSolve, buttonGenerate;
 
 	/*
 	 * (non-Javadoc)
@@ -47,221 +34,10 @@ public class SudokuGrid extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-		// We'll be creating an image that is 100 pixels wide and 200 pixels
-		// tall.
-		int width = 100;
-		int height = 200;
 
-		// Create a bitmap with the dimensions we defined above, and with a
-		// 16-bit pixel format. We'll
-		// get a little more in depth with pixel formats in a later post.
-		Bitmap bitmap = Bitmap.createBitmap(width, height, Config.RGB_565);
-
-		// Create a paint object for us to draw with, and set our drawing color
-		// to blue.
-		Paint paint = new Paint();
-		paint.setColor(Color.BLUE);
-
-		// Create a new canvas to draw on, and link it to the bitmap that we
-		// created above. Any drawing
-		// operations performed on the canvas will have an immediate effect on
-		// the pixel data of the
-		// bitmap.
-		Canvas canvas = new Canvas(bitmap);
-
-		// Fill the entire canvas with a red color.
-		canvas.drawColor(Color.RED);
-
-		// Draw a rectangle inside our image using the paint object we defined
-		// above. The rectangle's
-		// upper left corner will be at (25,50), and the lower left corner will
-		// be at (75,150). Since we set
-		// the paint object's color above, this rectangle will be blue.
-		canvas.drawRect(25, 50, 75, 150, paint);
-
-		// In order to display this image in our activity, we need to create a
-		// new ImageView that we
-		// can display.
-		ImageView imageView = new ImageView(this);
-
-		// Set this ImageView's bitmap to the one we have drawn to.
-		imageView.setImageBitmap(bitmap);
-
-		// Create a simple layout and add our image view to it.
-		RelativeLayout layout = new RelativeLayout(this);
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		params.addRule(RelativeLayout.CENTER_IN_PARENT);
-		layout.addView(imageView, params);
-		layout.setBackgroundColor(Color.BLACK);
-
-		// Show this layout in our activity.
-		setContentView(layout);
-=======
-		setContentView(R.layout.sudokugrid_main); // Set the layout.
-
-		game = new Game(9, 0);
-
-		MyGrid = (GridView) findViewById(R.id.MyGrid);
-		adapter = new VakjeAdapter(this);
-		MyGrid.setAdapter(adapter);
-
-		// buttons
-		button1 = (Button) findViewById(R.id.button1);
-		button2 = (Button) findViewById(R.id.button2);
-		button3 = (Button) findViewById(R.id.button3);
-		button4 = (Button) findViewById(R.id.button4);
-		button5 = (Button) findViewById(R.id.button5);
-		button6 = (Button) findViewById(R.id.button6);
-		button7 = (Button) findViewById(R.id.button7);
-		button8 = (Button) findViewById(R.id.button8);
-		button9 = (Button) findViewById(R.id.button9);
-		buttonClear = (Button) findViewById(R.id.buttonClear);
-		buttonSolve = (Button) findViewById(R.id.buttonSolve);
-		buttonGenerate = (Button) findViewById(R.id.buttonGenerate);
-
-		// TODO selection position naar x,y converteren en invullen bij
-		// listeners.
-		button1.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 1);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button2.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 2);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button3.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 3);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button4.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 4);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button5.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 5);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button6.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 6);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button7.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 7);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button8.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 8);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		button9.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 9);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		buttonClear.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				int position = adapter.getSelection();
-				int x = position % 9;
-				int y = position / 9;
-				game.setCurrentValue(x, y, 0);
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		buttonSolve.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// game.solveGame(); FIXME
-				adapter.notifyDataSetChanged();
-			}
-		});
-
-		buttonGenerate.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// game.generateGame(); FIXME
-				adapter.notifyDataSetChanged();
-			}
-		});
-
->>>>>>> b5be5a4389b1183b8c1d8bd29f2b311c48d1ae28
+		CanvasView view = new CanvasView(this);
+		setContentView(view);
+		view.requestFocus();
 	}
 
 	/**
@@ -319,11 +95,8 @@ public class SudokuGrid extends Activity {
 			int y = position / 9;
 
 			TextView tv = (TextView) MyView.findViewById(R.id.grid_item_text);
-<<<<<<< HEAD
-			// tv.setText("" + game.getCurrentValue(position, position));
-=======
+
 			tv.setText("" + game.getCurrentValue(x, y));
->>>>>>> b5be5a4389b1183b8c1d8bd29f2b311c48d1ae28
 
 			MyView.setTag(new Integer(position));
 
